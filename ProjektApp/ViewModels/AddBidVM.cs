50ef59756468a12f21536569fc6af7193b5d22b6
nooriiis <zaed_111@hotmail.com>
2022-10-20 20:34:49 +0200

    punk 4 + 6

---
+++ ProjektApp/ViewModels/AddBidVM.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectApp.ViewModels
+{
+    public class AddBidVM
+    {
+        [Required]
+        [Range(1, Int32.MaxValue, ErrorMessage = "Value should be greater than or equal to 1")]
+        public int Amount { get; set; }
+    }
+}
